5dcdc85ea2fe4190630d2666d614a40318819c78
Simfort <lemondzavadavid265@gmail.com>
2026-01-31 13:54:53 +0300

    add server

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import path from "node:path";
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(import.meta.dirname, "index.html"));
+});
+
+app.listen(3000, () => console.log("http://localhost:3000"));
