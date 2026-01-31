318284e475a69e5500b5e2bab0a8f2c73072120b
Simfort <lemondzavadavid265@gmail.com>
2026-01-31 17:01:54 +0300

    added workflow

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(import.meta.dirname, "index.html"));
+  res.sendFile(path.join(path.dirname(import.meta.dirname), "index.html"));
 });
 
 app.listen(3000, () => console.log("http://localhost:3000"));
